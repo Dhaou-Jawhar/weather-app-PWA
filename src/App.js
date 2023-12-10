fd430377791ca69228feeab57529966cb63a5fb5
Dhaou-Jawhar <dhaou.jawher@esprit.tn>
2023-12-10 14:17:13 +0100

    display simple <h1> text

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//fuction component fiha kelmet App bch nafficheha
+const App = () => {
+    return (
+        <h1>App</h1>
+        );
+}
+
+//export App : bch najjem n'importeha w nesta3melha fi index.js
+export default App;
