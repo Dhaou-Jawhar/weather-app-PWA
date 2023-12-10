06a860e78a452f34f66294a86ca7e0d3ce6ac0ae
Dhaou-Jawhar <dhaou.jawher@esprit.tn>
2023-12-10 17:11:27 +0100

    API -> Front ✅

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ const App = () => {
     //na3mlou function njibou fiha query ( search term ) w setquery ( set method le state field = query field ) 
     const [query, setQuery] = useState('');
 
+    //3malna const samineha weather w bch n7otou fiha e data eli raj3a mel API ( ba3d ma tenzel Enter )
+    const [weather, setWeather] = useState({});
+
     //bch njibou data mel API
     //ken nzelna Enter ba3d ma nektbou ( query = esm el bled )
     //y7ot esm el beld fel fonction mta3 el API w y7ot e reponse fi (data)
@@ -15,7 +18,8 @@ const App = () => {
         if (e.key === 'Enter') {
             const data = await fetchWeather(query);
 
-            console.log(data);
+            setWeather(data);
+            setQuery('');
     }
 }
     
@@ -30,6 +34,25 @@ const App = () => {
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyPress={search}
             />
+
+            {weather.main &&(
+                <div className='city'>
+                    <h2 className='city-name'>
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className='city-temp'>
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div>
+                        <div className='info'>
+                            <img className='city-icon' src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                            <p>{weather.weather[0].description}</p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
         );
 }
